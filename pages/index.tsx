e5f4784678869385bdebc94e9447000d0cbeb92c
treesha621 <riteshptl21@gmail.com>
2022-06-07 17:41:23 +0530

    Add chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,12 @@ import PieChart from "../components/chart/PieChart";
 import DoughnutChart from "../components/chart/DoughnutChart";
 import PolarAreaChart from "../components/chart/PolarAreaChart";
 import RadarChart from "../components/chart/RadarChart";
+import ScatterChart from "../components/chart/ScatterChart";
+import BubbleChart from "../components/chart/BubbleChart";
+import MultitypeChart from "../components/chart/MultitypeChart";
+import EventChart from "../components/chart/EventChart";
+import RefChart from "../components/chart/RefChart";
+import GradientChart from "../components/chart/GradientChart";
 
 export default function Home() {
     return (
@@ -47,7 +53,7 @@ export default function Home() {
                         <MultiaxisLineChart/>
                     </Paper>
                 </Grid>
-            
+
                 <Grid item xs={3}>
                     <Paper elevation={5} sx={{padding: '24px'}}>
                         <PieChart/>
@@ -66,6 +72,36 @@ export default function Home() {
                 <Grid item xs={3}>
                     <Paper elevation={5} sx={{padding: '24px'}}>
                         <RadarChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <ScatterChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <BubbleChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <MultitypeChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <EventChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <RefChart/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={5} sx={{padding: '24px'}}>
+                        <GradientChart/>
                     </Paper>
                 </Grid>
             </Grid>
