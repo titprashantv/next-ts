a4839244ee445f9581ce15c03af81fddc6746b82
treesha621 <riteshptl21@gmail.com>
2022-06-20 16:21:01 +0530

    Add login page

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Head from 'next/head';
-import {Box} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import theme from "../src/theme";
+import {FlutterDash} from "@mui/icons-material";
 
 interface Props {
     children?: React.ReactNode;
@@ -23,6 +24,11 @@ export default function AuthLayout(props: Props) {
                         <img src="/static/login.jpg"/>
                     </div>
                     <div className="auth-form-box">
+                        <Box>
+                            <Typography variant="h4" noWrap component="div" sx={{display: 'flex', alignItems: 'center', fontWeight:500}}>
+                                <FlutterDash color="secondary" sx={{marginRight: '10px', width: '80px', height: '80px'}}/> Analytics
+                            </Typography>
+                        </Box>
                         <div className="auth-form">
                             {children}
                         </div>
